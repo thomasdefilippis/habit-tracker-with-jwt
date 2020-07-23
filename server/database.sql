604f4f2b76bc868c1b8aa725bdcbde4543612d53
defi1673 <thomasdefilippis@gmail.com>
2020-07-23 11:39:56 -0700

    finished server

---
+++ server/database.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE habittracker;
+
+--extension required
+CREATE TABLE users(
+    user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_name VARCHAR(255) NOT NULL,
+    user_email VARCHAR(255) NOT NULL,
+    user_password VARCHAR(255) NOT NULL
+
+);
+
+--insert fake users
+
+INSERT INTO users (user_name, user_email, user_password)
+Values('thomas', 'thomasdefilippis@gmail.com', 'cloudyskies');
